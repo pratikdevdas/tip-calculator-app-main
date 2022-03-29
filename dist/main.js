54bf0145b23cffe4a377d77a801639e2c9867c02
Revenant <dasdev.pratik@gmail.com>
2022-03-29 18:24:32 +0530

    achieved👌:  js functionalities done

--- dist/main.js
+++ dist/main.js
@@ -1,23 +1,69 @@
-const billInput = document.querySelector('.billInput')
+let billInput = document.querySelector('.billInput')
+const personInput = document.querySelector('.peopleInput')
+const result = document.querySelector('#value1')
+const result2 = document.querySelector('#value2')
+const btn = document.getElementsByClassName('btn')
+const reset = document.querySelector('.reset')
+const custom = document.querySelector('.custom')
+let a,b=0,c,k,j,res;
 
-const result = document.querySelector('.result')
-billInput.addEventListener('input', updated);
+const buttonValue = (e) => {
+    b = Number(e.target.value)
+    calculate()
+    console.log(b)
+    return b
+}
 
-function updated (e) {
-    const val = result.textContent = e.target.value
-    console.log(val)
-    return val 
-    
+const billValue = (e) => {
+    a = Number(e.target.value)
+    calculate()
+    console.log(a)
+    return a
+}
+
+const personValue = (e) => {
+    c = Number(e.target.value)
+    calculate()
+    console.log(c)
+    return c
+}
+
+const resetter = () => {
+    a = 0
+    b = 0 
+    c = 0
+    calculate()
+    console.log('r')
+    return
+}
+
+btn[0].addEventListener('click', buttonValue)
+btn[1].addEventListener('click', buttonValue)
+btn[2].addEventListener('click', buttonValue)
+btn[3].addEventListener('click', buttonValue)
+btn[4].addEventListener('click', buttonValue)
+custom.addEventListener('input', buttonValue)
+
+billInput.addEventListener('input', billValue);
+personInput.addEventListener('input', personValue);
+
+reset.addEventListener('click', resetter)
+console.log()
+
+function calculate (){
+    res = a*b/100
+    console.log(a,b,c)
+    k = (res + a)/c
+    j = res/c;
+    if (isNaN(k) || k === Infinity  ){
+        result.textContent = '$0.00';
+        result2.textContent = '$0.00';
+        return
+    }
+    else {
+        result.textContent = `$${Math.round(j * 100)/100}`; 
+        result2.textContent = `$${Math.round(k * 100)/100}`;
+        return
+    }
 } 
 
-const btn1 = document.querySelector('.btn-1')
-const btn2 = document.querySelector('.btn-2')
-const btn3 = document.querySelector('.btn-3')
-const btn4 = document.querySelector('.btn-4')
-const btn5 = document.querySelector('.btn-5')
-
-btn1.addEventListener('onClick',()=>console.log('h'))
-// btn2.addEventListener('onClick'()=>{})
-// btn3.addEventListener('onClick'()=>{})
-// btn4.addEventListener('onClick'()=>{})
-// btn5.addEventListener('onClick'()=>{})
